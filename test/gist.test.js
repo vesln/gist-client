fa7dc3160fca197dce3f0dc7283b36f914f16f00
Veselin Todorov <veselin@veselin.bg>
2012-02-08 22:25:48 +0200

    Add file reader calls in Gist#publish.

--- test/gist.test.js
+++ test/gist.test.js
@@ -9,6 +9,8 @@
  * Support.
  */
 var should = require('chai').should();
+var jack = require('jack');
+var FileReader = require('../lib/file_reader');
 
 /**
  * The tested class.
@@ -19,10 +21,23 @@ var Gist = require('../lib/gist');
 
 describe('Gist', function() {
   describe('publish', function() {
-    it('should publish a gist', function() {
-      var gist = new Gist(['./tmp/foo.txt', './tmp/test.txt']);
+    it('should require file content', function(done) {
+      var gist = new Gist(['foo.txt', 'bar.txt']);
+      gist.fileReader.stub('read').and.replace(function(files) {
+        files.should.eql(['foo.txt', 'bar.txt']);
+        done();
+      });
+      gist.publish(function(err) {});
+    });
+    
+    it('should return error if there was an erro while reading files', function(done) {
+      var gist = new Gist(['foo.txt', 'bar.txt']);
+      gist.fileReader.stub('read').and.replace(function(files, cb) {
+        cb(new Error('Foo'));
+      });
       gist.publish(function(err) {
-        (err === null).should.be.true;
+        err.should.be.an.instanceof(Error);
+        done();
       });
     });
   });
