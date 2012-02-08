2ee2f1a95acf8d89a592137c41b62fdfadc8551f
Veselin Todorov <veselin@veselin.bg>
2012-02-08 21:41:48 +0200

    Add publish method.

--- test/gist.test.js
+++ test/gist.test.js
@@ -4,3 +4,26 @@
  * Veselin Todorov <hi@vesln.com>
  * MIT Licesnse.
  */
+ 
+/**
+ * Support.
+ */
+var should = require('chai').should();
+
+/**
+ * The tested class.
+ * 
+ * @type {Function}
+ */
+var Gist = require('../lib/gist');
+
+describe('Gist', function() {
+  describe('publish', function() {
+    it('should publish a gist', function() {
+      var gist = new Gist(['./tmp/foo.txt', './tmp/test.txt']);
+      gist.publish(function(err) {
+        (err === null).should.be.true;
+      });
+    });
+  });
+});
